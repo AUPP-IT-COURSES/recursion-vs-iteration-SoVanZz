f7107a5e2ea9aad82998ed6bbb6db3049e1711c5
SoVanZz <151815215+SoVanZz@users.noreply.github.com>
2023-11-23 14:04:54 +0700

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,15 +1,28 @@
 public class recursion {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        double[] xValues = {2.0, 3.0, 5.0, 6.0}; // Example x values
+        int[] nValues = {5, 3, 4, 2};           // Example n values
 
     //call your recursion fuction
-    //print result
+            double x = xValues[i];
+            int n = nValues[i];
 
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+            // Call your recursion function
+            double result = recursionFunction(x, n);
+
+            // Print the result
+            System.out.println(x + " to the power of " + n + " is:  " + result);
+        }
     }
-  
+
+    public static double recursionFunction(double x, int n) {
+        // Base case: x^0 is always 1
+        if (n == 0) {
+            return 1;
+        }
+        // Recursive case: x^n = x * x^(n-1)
+        else {
+            return x * recursionFunction(x, n - 1);
+        }
+    }
 }
-    
